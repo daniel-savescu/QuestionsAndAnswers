f7e123bcb7a1ccc106be4da97ac66816ddc34a7b
Daniel Savescu <savescu.daniel.cristian@gmail.com>
2024-10-04 16:39:57 +0300

    creating users page and test users

--- app.py
+++ app.py
@@ -38,7 +38,10 @@ def register():
         hashed_password = generate_password_hash(request.form['password'], method='pbkdf2:sha256')
         db.execute('insert into users (name, password, expert, admin) values (?, ?, ?, ?)',[request.form['name'], hashed_password, '0', '0'])
         db.commit()
-        return '<h1>User created</h1>'
+
+        session['user'] = request.form['name']
+
+        return redirect(url_for('index'))
     return render_template('register.html', user=user)
 
 @app.route('/login', methods=["GET", "POST"])
@@ -55,7 +58,7 @@ def login():
 
         if check_password_hash(user_result['password'], password):
             session['user'] = user_result['name']
-            return '<h1>The password is correct</h1>'
+            return redirect(url_for('index'))
         else:
             return '<h1>The password is incorrect</h1>'
 
@@ -86,7 +89,21 @@ def unanswered():
 @app.route('/users')
 def users():
     user = get_current_user()
-    return render_template('users.html', user=user)
+
+    db = get_db()
+    users_cur = db.execute('select id, name, expert, admin from users')
+    users_results = users_cur.fetchall()
+
+    return render_template('users.html', user=user, users=users_results)
+
+
+@app.route('/promote/<user_id>')
+def promote(user_id):
+    db = get_db()
+    db.execute('update users set expert = 1 where id = ?', [user_id])
+    db.commit()
+    return redirect(url_for('users'))
+
 
 @app.route('/logout')
 def logout():
