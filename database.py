e3da50839ee5a5e8f3dd9aec458e3e8f0bdc606d
Daniel Savescu <danielsavescu@Daniels-iMac.local>
2024-10-05 14:54:44 +0300

    changed database path

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from flask import g
 import sqlite3
 
 def connect_db():
-    sql = sqlite3.connect('/home/daniel/Python/QuestionsAndAnswers/questions.db')
+    sql = sqlite3.connect('/Users/danielsavescu/Python/QuestionsAndAnswers/questions.db')
     sql.row_factory = sqlite3.Row
     return sql
 
